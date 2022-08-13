f764a218d6ba1c38a14c70526583c511a579f478
Vlad <vlad.ricean@brivo.com>
2022-08-13 14:57:14 +0300

    -lifecycleScope

--- app/src/main/java/com/example/androidlearningproject/MainActivity.kt
+++ app/src/main/java/com/example/androidlearningproject/MainActivity.kt
@@ -1,7 +1,15 @@
 package com.example.androidlearningproject
 
+import android.content.Intent
+import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 val TAG = "MainActivity"
 
@@ -10,7 +18,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        val button = findViewById<Button>(R.id.btnStartActivity)
+        button.setOnClickListener {
+            lifecycleScope.launch {
+                while (true) {
+                    delay(1000L)
+                    Log.d(TAG, "Still running...")
+                }
+            }
+            GlobalScope.launch {
+                delay(5000L)
+                Intent(this@MainActivity, SecondActivity::class.java).also {
+                    startActivity(it)
+                    finish()
+                }
+            }
+        }
     }
 
 
