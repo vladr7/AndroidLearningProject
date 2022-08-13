b74650d369d6bb14a9cfc7cec603cf0d4ab2055b
Vlad <vlad.ricean@brivo.com>
2022-08-13 15:21:00 +0300

    -amazing coroutines with firestore

--- app/src/main/java/com/example/androidlearningproject/MainActivity.kt
+++ app/src/main/java/com/example/androidlearningproject/MainActivity.kt
@@ -2,15 +2,38 @@ package com.example.androidlearningproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.*
+import kotlinx.coroutines.tasks.await
 
-val TAG = "MainActivity"
+
+data class Person(
+    val name: String = "",
+    val age: Int = -1
+)
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val tutorialDocument = Firebase.firestore.collection("coroutines")
+            .document("tutorial")
+        val peter = Person("Peter", 25)
 
-        
+        GlobalScope.launch(Dispatchers.IO) {
+            delay(3000L)
+            tutorialDocument.set(peter).await()
+            val person = tutorialDocument.get().await().toObject(Person::class.java)
+            withContext(Dispatchers.Main) {
+                val tv = findViewById<TextView>(R.id.tvData)
+                tv.text = person.toString()
+            }
+        }
     }
 }
