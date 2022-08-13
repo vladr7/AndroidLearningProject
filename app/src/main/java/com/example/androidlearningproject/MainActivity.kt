d75dc41ff39ac74af2baea85095fc98d62f2d615
Vlad <vlad.ricean@brivo.com>
2022-08-13 14:04:58 +0300

    coroutine jobs, waiting, timeout

--- app/src/main/java/com/example/androidlearningproject/MainActivity.kt
+++ app/src/main/java/com/example/androidlearningproject/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.androidlearningproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val job = GlobalScope.launch(Dispatchers.Default) {
+            println("vlad Starting long running calculation...")
+            withTimeout(3000L) {
+                for(i in 30..40){
+                    if (isActive) {
+                        println("vlad Result for i = $i: ${fib(i)}")
+                    }
+                }
+            }
+
+            println("vlad Ending long running calculation...")
+        }
+    }
+
+    fun fib(n: Int): Long{
+        return if(n == 0) 0
+        else if(n == 1) 1
+        else fib(n - 1) + fib(n - 2)
     }
 }
