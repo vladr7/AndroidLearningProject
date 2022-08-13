bc9b568312ac0af0017706225723100bca3449c1
Vlad <vlad.ricean@brivo.com>
2022-08-13 15:40:29 +0300

    -get person/car tasks from firestore on same coroutine

--- app/src/main/java/com/example/androidlearningproject/MainActivity.kt
+++ app/src/main/java/com/example/androidlearningproject/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.androidlearningproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.*
@@ -14,26 +15,54 @@ data class Person(
     val age: Int = -1
 )
 
+data class Car(
+    val brand: String = "",
+    val year: Int = 2000
+)
+
 class MainActivity : AppCompatActivity() {
 
     val TAG = "MainActivity"
+    private val personDocument = Firebase.firestore.collection("coroutines")
+        .document("persoana")
+    private val carDocument = Firebase.firestore.collection("coroutines")
+        .document("car")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val tvData: TextView = findViewById<TextView>(R.id.tvData)
 
-        val tutorialDocument = Firebase.firestore.collection("coroutines")
-            .document("tutorial")
         val peter = Person("Peter", 25)
+        lifecycleScope.launch {
+            personDocument.set(peter)
+        }
 
-        GlobalScope.launch(Dispatchers.IO) {
-            delay(3000L)
-            tutorialDocument.set(peter).await()
-            val person = tutorialDocument.get().await().toObject(Person::class.java)
-            withContext(Dispatchers.Main) {
-                val tv = findViewById<TextView>(R.id.tvData)
-                tv.text = person.toString()
-            }
+        val car = Car("Mercedes", 2020)
+        lifecycleScope.launch {
+            carDocument.set(car)
+        }
+
+        // Task: print person got the car when both person/car jobs are finished
+        lifecycleScope.launch {
+            delay(2500L)
+            printGotTheCar(tvData)
         }
     }
+
+    private suspend fun printGotTheCar(tvData: TextView) {
+        val personResult = personDocument.get().await().toObject(Person::class.java)
+        val carResult = carDocument.get().await().toObject(Car::class.java)
+        tvData.text = "Person ${personResult?.name} got the car ${carResult?.brand}"
+    }
 }
+
+
+
+
+
+
+
+
+
+
