d2d0d2d125c309fc4f8804351fabc5cd84b8b3dc
Vlad <vlad.ricean@brivo.com>
2022-08-13 14:47:43 +0300

    -add lifecycle & viewmodelScope coroutines dependencies

--- app/src/main/java/com/example/androidlearningproject/MainActivity.kt
+++ app/src/main/java/com/example/androidlearningproject/MainActivity.kt
@@ -2,9 +2,6 @@ package com.example.androidlearningproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import kotlinx.coroutines.*
-import kotlin.system.measureTimeMillis
 
 val TAG = "MainActivity"
 
@@ -13,24 +10,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        GlobalScope.launch(Dispatchers.IO) {
-            val time = measureTimeMillis {
-                val answer1 = async { networkCall1() }
-                val answer2 = async { networkCall2() }
-                Log.d(TAG, "Answer1 is ${answer1.await()}")
-                Log.d(TAG, "Answer2 is ${answer2.await()}")
-            }
-            Log.d(TAG, "time: $time")
-        }
+
     }
 
-    suspend fun networkCall1() : String {
-        delay(3000L)
-        return "Answer 1"
-    }
 
-    suspend fun networkCall2() : String {
-        delay(3000L)
-        return "Answer 2"
-    }
 }
