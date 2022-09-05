f22b9ffa6568bb9bfdb458a2670f042f5157bd97
Vlad <ricean.vlad7@gmail.com>
2022-09-05 09:59:48 +0300

    -sync vs async
    https://developer.android.com/codelabs/basic-android-kotlin-training-introduction-coroutines?continue=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways%2Fandroid-basics-kotlin-unit-4-pathway-1%23codelab-https%3A%2F%2Fdeveloper.android.com%2Fcodelabs%2Fbasic-android-kotlin-training-introduction-coroutines#3

--- app/src/main/java/com/example/androidlearningproject/MainActivity.kt
+++ app/src/main/java/com/example/androidlearningproject/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.example.androidlearningproject
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import kotlinx.coroutines.*
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        runBlocking {
+            val num1 = async { getValue() }
+            val num2 = async { getValue() }
+            println("result of num1 + num2 is ${num1.await() + num2.await()}")
+        }
+        println("vlad: outside")
     }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    val formatter: DateTimeFormatter = DateTimeFormatter.ISO_LOCAL_TIME
+    @RequiresApi(Build.VERSION_CODES.O)
+    val time = { formatter.format(LocalDateTime.now()) }
+
+    suspend fun getValue(): Double {
+        println("vlad: entering getValue() at ${time()}")
+        delay(3000)
+        println("vlad: leaving getValue() at ${time()}")
+        return Math.random()
+    }
+
+
 }
